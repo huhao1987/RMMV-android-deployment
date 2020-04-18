c049df850457f8dbc579083741a5bd71867b6f01
hh <huhao1987@gmail.com>
2020-04-18 17:54:26 +1000

    Add file selection dialog

--- app/src/main/java/hh/rpgmakerplayer/MainActivity.kt
+++ app/src/main/java/hh/rpgmakerplayer/MainActivity.kt
@@ -1,8 +1,22 @@
 package hh.rpgmakerplayer
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.github.angads25.filepicker.controller.DialogSelectionListener
+import com.github.angads25.filepicker.model.DialogConfigs
+import com.github.angads25.filepicker.model.DialogProperties
+import com.github.angads25.filepicker.view.FilePickerDialog
+import hh.rpgmakerplayer.SDCardsUtils.dialog
+import hh.rpgmakerplayer.SDCardsUtils.initFolderSelection
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +24,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         webplayview.Playgame(getString(R.string.mv_project_index))
+        initFolderSelection(this)
+    }
+
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            when(requestCode) {
+                FilePickerDialog.EXTERNAL_READ_PERMISSION_GRANT -> {
+                    if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        if (dialog != null) {
+                            //Show dialog if the read permission has been granted.
+                            dialog!!.show();
+                        }
+                    } else {
+                        //Permission has not been granted. Notify the user.
+                        dialog!!.dismiss()
+                    }
+                }
+            }
     }
 }
